010a03069c314ddc8db1a8eb62b17aae2584c390
luongdoan277 <luongdoan277@gmail.com>
2020-12-28 12:12:21 +0700

    up

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace Admin
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
             });
         }
     }
