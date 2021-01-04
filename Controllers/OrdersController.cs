4f140d26e7e210b3fca1433856f377b4c6e688b5
luongdoan277 <luongdoan277@gmail.com>
2021-01-04 17:57:09 +0700

    up ordercontroller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,108 @@
+﻿using Admin.Data;
+using Admin.Models;
+using Admin.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly AdminContext _context;
+
+        public OrdersController(AdminContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var storeDbContext = _context.Orders.Include(p => p.Customer);
+            return View(await storeDbContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            OrderListViewModel order = new OrderListViewModel
+            {
+                Orders = _context.Orders.Where(o => o.OrderID == id).Include(o => o.Customer),
+                OrderItems = _context.OrderItems.Where(o => o.OrderID == id).Include(o => o.Product),
+                //Customer = _context.
+            };
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderID,OrderOPP,CustomerID,OrderAddress,ShipMethod,PaymentMethod,PaymentStatus,dateTime,OrderStatus,TotalPrice")] Order order)
+        {
+            if (id != order.OrderID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerID"] = new SelectList(_context.Customers, "CustomerID", "CustomerID", order.CustomerID);
+            ViewData["OrderItemID"] = new SelectList(_context.OrderItems, "OrderItemID", "OrderItemID", order.OrderID);
+            return View(order);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderID == id);
+        }
+    }
+}
